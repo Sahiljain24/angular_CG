a76d1412ebad52694824e38e962bd977af523fe1
Sahiljain24 <24sahilj@gmail.com>
2025-03-06 16:49:39 +0530

    [sahil] Add. added all the files for uc_04

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
@@ -11,6 +12,7 @@ export class AppComponent {
   title = 'Helloworld';
   message : string ="HEllo from bridgelabz";
   imageURL: string="assets/logoBridge.jpg";
+  userName: string = ""; 
   openBridgelabzSite() : void{
       window.open("https://www.bridgelabz.com", "_blank")
   }
