392801203855438a7ceefbf78b10495389c4f4ec
Sahiljain24 <24sahilj@gmail.com>
2025-03-07 11:22:43 +0530

    [sahil] Add. added all the files for uc_05

--- Helloworld/src/app/app.component.ts
+++ Helloworld/src/app/app.component.ts
@@ -1,20 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , FormsModule],
+  standalone: true, 
+  imports: [RouterOutlet, FormsModule], // ✅ Ensure FormsModule is imported
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Helloworld';
-  message : string ="HEllo from bridgelabz";
-  imageURL: string="assets/logoBridge.jpg";
+  message: string = "Hello from BridgeLabz";
+  imageURL: string = "assets/logoBridge.jpg";
   userName: string = ""; 
-  openBridgelabzSite() : void{
-      window.open("https://www.bridgelabz.com", "_blank")
+  errorMessage: string = "";
+
+  validateUserName($event:any) {
+   const nameRegex = RegExp('^[A-Z][A-za-z\\s]{2,}$')
+   if (nameRegex.test(this.userName)) {
+    this.errorMessage=""
+    return;
+   }
+   
+    this.errorMessage="Name is incorrect";
+   
+  }
+
+  openBridgelabzSite(): void {
+    window.open("https://www.bridgelabz.com", "_blank");
   }
 }
-
